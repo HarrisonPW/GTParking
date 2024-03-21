7fcfb611656c673ca59324b322f841defad43e4f
zafc <jzhang7256@gmail.com>
2024-03-21 18:59:21 -0400

    wip userserviceimpl

---
+++ GTParking-Service/src/main/java/org/GTParking/dao/UserDao.java
@@ -0,0 +1,30 @@
+package org.GTParking.dao;
+
+import org.GTParking.entity.po.User;
+import org.apache.ibatis.annotations.Param;
+import java.util.List;
+
+public interface UserDao {
+    User queryById(Integer userid);
+
+
+    List<User> queryAllByLimit(@Param("po")User sysUser, @Param("pageNo") Long pageNo, @Param("pageSize") Long pageSize);
+
+
+    long count(User user);
+
+
+    int insert(User user);
+
+
+    int insertBatch(@Param("entities") List<User> entities);
+
+
+    int insertOrUpdateBatch(@Param("entities") List<User> entities);
+
+
+    int update(User user);
+
+
+    int deleteById(Integer userid);
+}
--- GTParking-Service/src/main/java/org/GTParking/service/impl/UserServiceImpl.java
+++ GTParking-Service/src/main/java/org/GTParking/service/impl/UserServiceImpl.java
@@ -1,8 +1,52 @@
 package org.GTParking.service.impl;
 
+import org.GTParking.dao.ParkinglotsDao;
+import org.springframework.stereotype.Service;
+import org.GTParking.bean.PageResponse;
+import org.GTParking.entity.po.User;
+import org.GTParking.dao.UserDao;
+import org.GTParking.entity.request.UserRequest;
+import org.GTParking.service.UserService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
-@Service("UserService")
-public class UserServiceImpl {
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
 
+@Service("UserService")
+public class UserServiceImpl implements UserService{
+
+    @Resource
+    private UserDao userDao;
+
+    @Override
+    public User queryUserVById(Integer userid){
+
+    }
+
+    @Override
+    public PageResponse<User> queryUserByPage(UserRequest userRequest){
+
+    }
+
+    @Override
+    public User insertUser(User user){
+
+    }
+
+    @Override
+    public User updateUser(User user){
+
+    }
+
+    @Override
+    public boolean deleteById(Integer userid){
+
+    }
+
+    @Override
+    public boolean updateLocation(Integer userid, Date timestamp, Double latitude, Double longitude){
+
+    }
 }
